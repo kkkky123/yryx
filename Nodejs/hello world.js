88e1065428a079150f81f29e2d479add2a3d5adf
yankezhong <neveryan211@163.com>
2015-04-23 00:13:05 +0800

    node start

---
+++ Nodejs/hello world.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ykz on 2015/4/22.
+ */
+var http=require("http");
+http.createServer(function(req,res){
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    res.end("hello world");
+}).listen(8080,"127.0.0.1");
+console.log("Server start");
