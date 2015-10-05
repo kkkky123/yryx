22db7b41335fc1bf90bf3930bd64aa6f6f386e97
yankezhong <neveryan211@163.com>
2015-10-05 17:20:25 +0800

    add react test

--- es5Js/event.js
+++ es5Js/event.js
@@ -2,54 +2,54 @@
  * Created by ykz on 2014/11/17.
  */
 var EventUtil = {
-    getEvent:function(event){
-        return event?event:window.event;
+    getEvent: function(event) {
+        return event ? event : window.event;
     },
-    getTarget:function(event){
+    getTarget: function(event) {
         return event.target || event.srcElement;
     },
-    preventDefault:function(event){
-        if(event.preventDefault){
+    preventDefault: function(event) {
+        if (event.preventDefault) {
             event.preventDefault();
-        }else{
-            event.returnValue=false;
+        } else {
+            event.returnValue = false;
         }
     },
-    stopPropagation:function(event){
-        if(event.stopPropagation){
+    stopPropagation: function(event) {
+        if (event.stopPropagation) {
             event.stopPropagation();
-        }else{
-            event.cancelBubble=true;
+        } else {
+            event.cancelBubble = true;
         }
     },
-    getRelatedTarget:function(event){
+    getRelatedTarget: function(event) {
         /*获取相关元素，只对mousover和mouseout有用，对mouseover来说，被移入的是主体元素，移除的是相关元素，mouseout相反*/
-        if(event.relatedTarget){
+        if (event.relatedTarget) {
             return event.relatedTarget;
-        }else if(event.toElement){
+        } else if (event.toElement) {
             return event.toElement;
-        }else if(event.fromElement){
+        } else if (event.fromElement) {
             return fromElement;
-        }else{
+        } else {
             return null;
         }
     },
-    addHandler:function(element,type,handler){
-        if(element.addEventListener){
-            element.addEventListener(type,handler,false);
-        }else if(element.attachEvent){
-            element.attachEvent("on"+type,handler);
-        }else{
-            element["on"+type]=handler;
+    addHandler: function(element, type, handler) {
+        if (element.addEventListener) {
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
         }
     },
-    removeHandler:function(element,type,handler){
-        if(element.removeEventListener){
-            element.removeEventListener(type,handler,false);
-        }else if(element.detachEvent){
-            element.detachEvent("on"+type,handler);
-        }else{
-            element["on"+type]=null;
+    removeHandler: function(element, type, handler) {
+        if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent) {
+            element.detachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = null;
         }
     }
 };
---
+++ react/test/src/comment_test.js
@@ -0,0 +1,104 @@
+/**
+ * Created by ykz on 2015/9/27.
+ */
+var CommentBox = React.createClass({
+  getInitialState:function() {
+    return {data: []};
+  },
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  handleCommentSubmit: function(comment) {
+  	var comments = this.state.data;
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    return ( 
+    	<div className = "commentBox">
+          <h1> Comments </h1>
+          < CommentList data = { this.state.data }/>
+          < CommentForm onCommentSubmit={ this.handleCommentSubmit } />
+      </div>
+      );
+  }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+          return ( 
+          	< Comment author = { comment.author } > { comment.text } </Comment>
+          	);
+        });
+        return ( 
+        	< div className = "commentList" > { commentNodes } </div>
+        	);
+    }
+});
+
+var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.refs.author.getDOMNode().value.trim();
+    var text = this.refs.text.getDOMNode().value.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    this.refs.author.getDOMNode().value = '';
+    this.refs.text.getDOMNode().value = '';
+    return;
+  },
+  render: function() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+var converter = new Showdown.converter();
+var Comment = React.createClass({
+    render: function() {
+        var rawMarkup = converter.makeHtml(this.props.children.toString());
+        return ( 
+        	< div className = "comment" >
+            < h2 className = "commentAuthor" > { this.props.author } </h2>
+            /*{this.props.children}*/
+            < span dangerouslySetInnerHTML = { { __html: rawMarkup } } /> 
+          </div>
+        );
+    }
+})
+React.render(
+    <CommentBox url = "http://localhost:63342/angularJs/react-0.13.0/test/src/comments.json" pollInterval="2000" />,
+  document.getElementById('content')
+);
